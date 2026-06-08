1411b29669b74745664012c1513966f8b798d515
Tanveer Ahmed <bdtanveerahmed@gmail.com>
2026-06-08 16:17:44 -0400

    Update greetings message - Tanveer Ahmed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py - Created by Tanveer Ahmed
 from unicodedata import name
 def greet(name):
-    return f"Hello, {name}! Welcome to AIDI 2004."
+    return f"Hello, {name}! Welcome to AIDI 2004. I hope you have a great semester learning about AI and its applications."
 
 if __name__ == "__main__":
     print(greet("Tanveer Ahmed"))
